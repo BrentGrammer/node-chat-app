61bb435a1b4867d696583ba0c8014cbf75088e8a
BrentGrammer <brentmarquez@gmail.com>
2018-12-30 02:21:01 -0700

    add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -11,15 +11,30 @@
       })
     
       socket.on('newMessage', function(message) {
-        console.log('new message', message)
+        console.log('new message', message);
+        // append chat message to ol on index page:
+        const li = $('<li></li>');
+        li.text(`${message.from}: ${message.text}`);
+
+        $('#messages').append(li);
       });
 
-      // third arg is defining a callback that will be used on the listener in the server for acknowledgement.
-      socket.emit('createMessage', {
-        from: "Frank",
-        text: "hi"
-      }, function() {
-        console.log('got it')
+      // // third arg is defining a callback that will be used on the listener in the server for acknowledgement.
+      // socket.emit('createMessage', {
+      //   from: "Frank",
+      //   text: "hi"
+      // }, function(data) {
+      //   console.log('got it', data)
+      // })
+
+      $('#message-form').on('submit', function(e) {
+        e.preventDefault();
+        socket.emit('createMessage', {
+          from: "user",
+          text: $('[name=message]').val()
+        }, function() {
+
+        })
       })
     });
 
